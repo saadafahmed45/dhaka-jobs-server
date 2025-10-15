cacf2b0b834c993d207d16a7868aa11d64d7f48c
saadafahmed45 <hafijnahid45@gmail.com>
2025-10-16 03:43:39 +0600

    added the my pc ip access

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(express.json());
 //Must remove "/" from your production URL
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://dhaka-jobs.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "192.168.0.108:3000",
+      "https://dhaka-jobs.vercel.app",
+    ],
     credentials: true,
   })
 );
